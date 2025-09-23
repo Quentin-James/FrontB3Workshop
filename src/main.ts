64b7e74e9cf41a5021978576ec2e015e18227081
Eliott Augereau le best, le roi de la mort qui tue <eaugereau@la-joliverie.com>
2025-09-23 14:51:48 +0200

    architecture de base + ajout de NG charts pour l'affichage de graphiques

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { mergeApplicationConfig } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+const config = mergeApplicationConfig(appConfig, {
+  providers: [
+    provideCharts(withDefaultRegisterables())
+  ]
+});
+
+bootstrapApplication(App, config)
   .catch((err) => console.error(err));
